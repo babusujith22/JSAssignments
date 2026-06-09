92130ce23c2c6f08806cb2fa834bbcd779516b18
babusujith22 <60853772+babusujith22@users.noreply.github.com>
2026-06-09 23:27:50 +0530

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,17 @@
+let userName ='Sujith'
+let organization ='IBM'
+let contactNumber = 8220276796
+let knowsAutomation = true
+let usesPlaywright
+
+console.log(userName);
+console.log(organization);
+console.log(contactNumber);
+console.log(knowsAutomation);
+console.log(usesPlaywright);
+
+console.log(typeof userName);
+console.log(typeof organization);
+console.log(typeof contactNumber);
+console.log(typeof knowsAutomation);
+console.log(typeof usesPlaywright);
---
+++ variableScoping.js
@@ -0,0 +1,10 @@
+const browserVersion ='chrome'
+function getBrowserVersion(){
+    if (browserVersion === 'chrome'){
+        let browserVersion = '10.1'
+
+    }
+console.log(browserVersion);
+
+}
+getBrowserVersion()
